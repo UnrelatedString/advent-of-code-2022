7092f74d82d0a9bca36ab4f412e595011d0d75b0
UnrelatedString <unrelatedstring@gmail.com>
2022-12-04 00:33:13 -0500

    septic tank type hints

--- septic_tank.py
+++ septic_tank.py
@@ -1,15 +1,21 @@
+from typing import List
+from collections.abc import Iterable
+
 import string
 import more_itertools as mi
 
 alpha = string.ascii_lowercase
 
-def slorp():
+def slorp() -> Iterable[str]:
     return (line.strip('\n') for line in open(0))
 
-def line_groups(lines):
+def line_groups(lines: Iterable[str]) -> List[List[str]]:
     return [group.split('\n') for group in '\n'.join(lines).strip('\n').split('\n\n')]
 
-def serial_split(string, delimiters):
+'''
+Recursively split on a series of delimiters
+'''
+def serial_split(string: str, delimiters: Iterable[str]):
     if delimiters:
         head, *tail = delimiters
         return (serial_split(slice, tail) for slice in string.split(head))
