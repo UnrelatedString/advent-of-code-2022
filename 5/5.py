0fd0009e6cbbc32c432dbc40b3f601582275a529
UnrelatedString <unrelatedstring@gmail.com>
2022-12-05 00:14:18 -0500

    hooray for off by 2s
    also the fucking type hints errored somehow

---
+++ 5/5.py
@@ -0,0 +1,19 @@
+# from ..septic_tank import * # why can't we have good things
+exec(open('../septic_tank.py').read())
+
+crates, moves = line_groups(slorp())
+
+stacks = [[] for _ in range(9)]
+for line in crates[:-1][::-1]:
+    row = line[1::4]
+    for stack, crate in zip(stacks, row):
+        if crate != ' ':
+            stack.append(crate)
+
+for line in moves:
+    q,f,t = map(int,line.split()[1::2])
+    for _ in range(q):
+        if stacks[f-1]:
+            stacks[t-1].append(stacks[f-1].pop())
+
+print(''.join(s[-1] for s in stacks if s))
--- septic_tank.py
+++ septic_tank.py
@@ -6,16 +6,16 @@ import more_itertools as mi
 
 alpha = string.ascii_lowercase
 
-def slorp() -> Iterable[str]:
+def slorp():
     return (line.strip('\n') for line in open(0))
 
-def line_groups(lines: Iterable[str]) -> List[List[str]]:
+def line_groups(lines) -> List[List[str]]:
     return [group.split('\n') for group in '\n'.join(lines).strip('\n').split('\n\n')]
 
 '''
 Recursively split on a series of delimiters
 '''
-def serial_split(string: str, delimiters: Iterable[str]):
+def serial_split(string: str, delimiters):
     if delimiters:
         head, *tail = delimiters
         return (serial_split(slice, tail) for slice in string.split(head))
