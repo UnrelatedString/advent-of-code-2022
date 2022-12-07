cfcffe9de79e347e84fb4b06924e2e2f7b488a0d
UnrelatedString <unrelatedstring@gmail.com>
2022-12-07 00:22:37 -0500

    wow

---
+++ 7/7.py
@@ -0,0 +1,46 @@
+# from ..septic_tank import * # why can't we have good things
+exec(open('../septic_tank.py').read())
+
+dirs = {}
+current = ['/']
+
+for line in slorp():
+    if not line: pass
+    if tuple(current) not in dirs:
+        dirs[tuple(current)] = set()
+    x = dirs[tuple(current)]
+    if line.startswith('$ cd'):
+        t = line[5:]
+        if t == '/':
+            current = [t]
+        elif t == '..':
+            current.pop()
+        else:
+            current.append(t)
+    elif line.startswith('$ ls'):
+        pass # lmao
+    elif line.startswith('dir '):
+        t = line[4:]
+        x.add(t)
+    else:
+        s, n = line.split()
+        x.add((int(s), n))
+
+sizes = {}
+
+def measure(dir):
+    if dir in sizes:
+        return sizes[dir]
+    size = 0
+    for e in dirs[dir]:
+        if type(e) is str:
+            size += measure((*dir, e))
+        else:
+            size += e[0]
+    sizes[dir] = size
+    return size
+
+measure(('/',))
+
+#print(sizes)
+print(sum(sizes[dir] for dir in sizes if sizes[dir] <= 100000))
